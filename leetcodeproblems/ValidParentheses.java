dc19980acd654d90ce0c84fd573b1bc343670915
Aishwarya-V2004 <aishwaryav096@gmail.com>
2025-04-24 22:45:40 +0530

    24-04-2025

---
+++ leetcodeproblems/ValidParentheses.java
@@ -0,0 +1,31 @@
+package leetcodeproblems;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                stack.push(')');
+            } else if (ch == '{') {
+                stack.push('}');
+            } else if (ch == '[') {
+                stack.push(']');
+            } else {
+                if (stack.isEmpty() || stack.pop() != ch) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));
+        System.out.println(isValid("()[]{}"));
+        System.out.println(isValid("(]"));
+        System.out.println(isValid("([])"));
+    }
+}
