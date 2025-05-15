3999f70b95579c3af3069f7c5c9e5346c1a45d5c
AISHWARYA V <aishwaryav096@gmail.com>
2025-05-15 23:31:44 +0530

    15-05-2025

---
+++ arrays_leetcode/GroupAnagrams.java
@@ -0,0 +1,35 @@
+package arrays_leetcode;
+
+import java.util.*;
+
+//Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+//Example 1:
+//Input: strs = ["eat","tea","tan","ate","nat","bat"]
+//Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+//Explanation:
+//There is no string in strs that can be rearranged to form "bat".
+//The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+//The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+//------------------------------------------------------------------------------------------------------------------
+public class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> anagramMap = new HashMap<>();
+
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+
+            anagramMap.computeIfAbsent(sortedStr, k -> new ArrayList<>()).add(str);
+        }
+
+        return new ArrayList<>(anagramMap.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> result = groupAnagrams(strs);
+
+        System.out.println(result);
+    }
+}
