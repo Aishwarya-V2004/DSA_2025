adfbbb4674d84f80358c374822fc316d567ce4f1
AISHWARYA V <aishwaryav096@gmail.com>
2025-07-12 23:48:21 +0530

    12-07-2025

---
+++ arrays_leetcode/Searchinsertposition.java
@@ -0,0 +1,42 @@
+package arrays_leetcode;
+
+
+//Given a sorted array of distinct integers and a target value, return the index if the target is found.
+// If not, return the index where it would be if it were inserted in order.
+//You must write an algorithm with O(log n) runtime complexity.
+//Example 1:
+//Input: nums = [1,3,5,6], target = 5
+//Output: 2
+//Example 2:
+//Input: nums = [1,3,5,6], target = 2
+//Output: 1
+//Example 3:
+//Input: nums = [1,3,5,6], target = 7
+//Output: 4
+// ------------------------------------------------------------------------------------------------------------
+
+public class Searchinsertposition {
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return left; // insertion point
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 3, 5, 6};
+        System.out.println(searchInsert(nums1, 5)); // Output: 2
+        System.out.println(searchInsert(nums1, 2)); // Output: 1
+        System.out.println(searchInsert(nums1, 7)); // Output: 4
+    }
+}
