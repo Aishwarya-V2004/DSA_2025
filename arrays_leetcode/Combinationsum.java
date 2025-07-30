2508589826db81d2383b1d7c51cf83e6307981f5
AISHWARYA V <aishwaryav096@gmail.com>
2025-07-30 23:21:59 +0530

    30-07-2025

---
+++ arrays_leetcode/Combinationsum.java
@@ -0,0 +1,65 @@
+package arrays_leetcode;
+
+//Given an array of distinct integers candidates and a target integer target, return a list of all unique
+// combinations of candidates where the chosen numbers sum to target. You may return the combinations in
+// any order.
+//The same number may be chosen from candidates an unlimited number of times. Two combinations are unique
+// if the frequency of at least one of the chosen numbers is different.
+//The test cases are generated such that the number of unique combinations that sum up to target
+// is less than 150 combinations for the given input.
+//Example 1:
+//Input: candidates = [2,3,6,7], target = 7
+//Output: [[2,2,3],[7]]
+//Explanation:
+//2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+//7 is a candidate, and 7 = 7.
+//These are the only two combinations.
+//Example 2:
+//Input: candidates = [2,3,5], target = 8
+//Output: [[2,2,2,2],[2,3,3],[3,5]]
+//Example 3:
+//Input: candidates = [2], target = 1
+//Output: []
+//------------------------------------------------------------------------------------------------------------
+
+import java.util.*;
+
+public class Combinationsum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(candidates, target, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int[] candidates, int target, int startIndex, List<Integer> current, List<List<Integer>> result) {
+        if (target == 0) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        if (target < 0) {
+            return;
+        }
+
+        for (int i = startIndex; i < candidates.length; i++) {
+            current.add(candidates[i]);
+            // Allow same element again, so pass 'i' instead of 'i+1'
+            backtrack(candidates, target - candidates[i], i, current, result);
+            current.remove(current.size() - 1); // backtrack
+        }
+    }
+    public static void main(String[] args) {
+        Combinationsum sol = new Combinationsum();
+        int[] candidates1 = {2, 3, 6, 7};
+        int target1 = 7;
+        System.out.println(sol.combinationSum(candidates1, target1));
+
+        int[] candidates2 = {2, 3, 5};
+        int target2 = 8;
+        System.out.println(sol.combinationSum(candidates2, target2));
+
+        int[] candidates3 = {2};
+        int target3 = 1;
+        System.out.println(sol.combinationSum(candidates3, target3));
+    }
+}
